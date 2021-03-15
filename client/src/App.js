a68207a6932b588947fb3c46dc29255a07aa1d0d
Nikolay Mihaylov <n.mihaylov@abv.bg>
2021-03-15 08:39:09 +0200

    Add Aside and main components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import style from './App.module.css';
 import Header from './components/Header/';
+import Menu from './components/Menu/';
+import Main from './components/Main/';
 
 function App() {
   return (
     <div className={style.app}>
-      <h1>Lets Begin</h1>
-
       <Header />
+      <div className={style.container}>
+        <Menu />
+
+        <Main />
+      </div>
     </div>
   );
 }
---
+++ client/src/components/Main/Main.js
@@ -0,0 +1,17 @@
+import { Fragment } from 'react';
+import './Main.css';
+
+
+const MenuItem = ({
+    children
+}) => {
+    return (
+        <main className="main-container">
+            <h1>Some Headiing</h1>
+
+            <p>Some posts</p>
+        </main>
+    )
+}
+
+export default MenuItem;
